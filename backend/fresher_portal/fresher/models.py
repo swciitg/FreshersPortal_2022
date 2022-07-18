005e5a181bde0cf11cdec500ac80d70e632ce087
Chetan Chinchulkar <76626599+Chetan-Chinchulkar@users.noreply.github.com>
2022-07-18 09:49:54 +0530

    dropmsg model deleted

--- backend/fresher_portal/fresher/models.py
+++ backend/fresher_portal/fresher/models.py
@@ -32,14 +32,3 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# drop your msg forms here
-class DropMsg(models.Model):
-    name = models.CharField(max_length=100, null=True, blank=True)
-    email = models.EmailField(max_length=100, null=True, blank=True)
-    phone = models.CharField(max_length=10, null=True, blank=True)
-    msg = models.TextField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name

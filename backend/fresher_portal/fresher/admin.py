808e3db23b3806a1aed223c36a1cb9111da88a39
ayushavi <ayushavi.123@gmail.com>
2022-07-19 12:16:15 +0530

    Revert "Creating a fresh branch"
    This reverts commit d69b674e57a02ba31e21dbe09dab46428bc40fdf.

---
+++ backend/fresher_portal/fresher/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+admin.site.register(BaseModel)
+admin.site.register(Imp)
+admin.site.register(Contact)
+admin.site.register(Open1)
+admin.site.register(Open2)
+admin.site.register(FAQ)
+

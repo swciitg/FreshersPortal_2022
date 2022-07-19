ce7e870dacf56e767a2595ca4ed373350474f613
Chetan Chinchulkar <76626599+Chetan-Chinchulkar@users.noreply.github.com>
2022-07-19 13:42:39 +0530

    tried API

--- backend/fresher_portal/fresher/views.py
+++ backend/fresher_portal/fresher/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 
 from .forms import DropMsgForm
 # Create your views here.
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import GroupSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer

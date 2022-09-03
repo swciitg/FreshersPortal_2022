0fe202509cbebfccc9bd11ce9b8fa31d27e02414
demongod11 <c.chandrabhushanreddy@gmail.com>
2022-09-03 18:45:41 +0530

    Added cors to backend

--- backend/fresher_portal/fresher_portal/settings.py
+++ backend/fresher_portal/fresher_portal/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL=True
 
 # Application definition
 
@@ -41,9 +42,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'fresher',
     'ckeditor',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

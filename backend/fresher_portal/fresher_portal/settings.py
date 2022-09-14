2185ac9496fb19384efd4320d5b55ec017f6b6aa
demongod11 <c.chandrabhushanreddy@gmail.com>
2022-09-14 22:41:11 +0530

    Bug in backend cors

--- backend/fresher_portal/fresher_portal/settings.py
+++ backend/fresher_portal/fresher_portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2v38fh1u2@n%*1(wk=*8pwzcy*h^cc2v(6dk4^n4ox^i2_-cea
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL=True
 

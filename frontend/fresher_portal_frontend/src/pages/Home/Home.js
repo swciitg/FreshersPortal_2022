16155903d43d2870f4a30f4db4e323331dc21e85
ayushavi <ayushavi.123@gmail.com>
2022-09-29 21:28:55 +0530

    A few changes in home page and contact us page

--- frontend/fresher_portal_frontend/src/pages/Home/Home.js
+++ frontend/fresher_portal_frontend/src/pages/Home/Home.js
@@ -1,26 +1,26 @@
-import * as React from "react";
-import "./Home.css";
-import { NavLink } from "../../components/Navbar/NavbarElememts";
+import React from 'react';
+import './Home.css';
+import { NavLink } from '../../components/Navbar/NavbarElememts';
 
 export default function Home() {
   return (
-      <div
-        id="container"
-        // style={{
-        //   backgroundImage: `url(/AdmittedStudents/guesthouse1.jpg)`
-        // }}
-      >
-        <img src="/AdmittedStudents/guesthouse1.jpg" alt="" />
-        <div id="trs">
-          <div id="d1">
-            <div id="ha">WELCOME TO</div>
-            <div id="hb">IIT GUWAHATI</div>
-          </div>
-
-          <NavLink to="/admittedstudent" id="AS">
-            Admitted Student
-          </NavLink>
+    <div
+      id='container'
+      // style={{
+      //   backgroundImage: `url(/AdmittedStudents/guesthouse1.jpg)`
+      // }}
+    >
+      <img className='home_img' src='/AdmittedStudents/guesthouse1.jpg' alt='' />
+      <div id='trs'>
+        <div id='d1'>
+          <div id='ha'>WELCOME TO</div>
+          <div id='hb'>IIT GUWAHATI</div>
         </div>
+
+        <NavLink to='/admittedstudent' id='AS'>
+          Admitted Student
+        </NavLink>
       </div>
+    </div>
   );
 }

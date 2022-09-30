283023d0d40c5a962a4daf28f1868858b121c010
demongod11 <c.chandrabhushanreddy@gmail.com>
2022-09-30 20:05:26 +0530

    Populated db

--- backend/fresher_portal/fresher/urls.py
+++ backend/fresher_portal/fresher/urls.py
@@ -7,13 +7,6 @@ app_name='fresher'
 
 urlpatterns = [
     path('api/<str:var1>/<str:var2>',views.API),
-    # path('api/imp/<int:var>/', API.as_view(),name='api-imp'),
-    # path('api/imp/', API.as_view(),name='api-imp'),
-    # path('api/base', API.as_view(var=2),name='api-base'),
-    # path('api/contact', API.as_view(var=3),name='api-contact'),
-    # path('api/open1', API.as_view(var=4),name='api-open1'),
-    # path('api/open2', API.as_view(var=5),name='api-open2'),
-    # path('api/faq', API.as_view(var=6),name='api-faq'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
